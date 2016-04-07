114861f393e5a5dd47374d8a1d5a547400cfcd21
Praženica Andrej <andrej@prazenica.com>
2016-04-07 14:26:21 +0200

    initial commit

---
+++ myControllers/myControllers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  myControllers
+//
+//  Created by Praženica Andrej on 07/04/16.
+//  Copyright © 2016 Praženica Andrej. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
